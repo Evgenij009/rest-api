4638c6fa5a416c500885e7ac710979c116d5deb0
Evgenij009 <evgeshashadura@gmail.com>
2021-12-10 00:29:37 +0300

    add javadoc

--- src/main/java/com/epam/esm/dao/GiftCertificateDao.java
+++ src/main/java/com/epam/esm/dao/GiftCertificateDao.java
@@ -7,15 +7,81 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * DAO interface for Gift certificate.
+ */
 public interface GiftCertificateDao {
+    /**
+     * Create new Certificate.
+     *
+     * @param giftCertificate the gift certificate
+     * @return the long id new certificate
+     */
     long create(GiftCertificate giftCertificate);
+
+    /**
+     * Find all certificates.
+     *
+     * @return the list of certificates
+     */
     List<GiftCertificate> findAll();
+
+    /**
+     * Gets all Certificates with sorting.
+     *
+     * @param sortParamsContext sort parameters
+     * @return List of sorted Certificates
+     */
     List<GiftCertificate> findAllWithSorting(SortParamsContext sortParamsContext);
+
+    /**
+     * Gets all Certificates with filtering.
+     *
+     * @param ids      ids for filtering Certificates
+     * @param partInfo part info of name/description of Certificate to filter
+     * @return List of filtered Certificates
+     */
     List<GiftCertificate> findAllWithFiltering(List<Long> ids, String partInfo);
+
+    /**
+     * Gets all Certificates with sorting and filtering.
+     *
+     * @param sortParamsContext sort parameters
+     * @param ids      ids for filtering Certificates
+     * @param partInfo part info of name/description of Certificate to filter
+     * @return List of filtered and sorted Certificates
+     */
     List<GiftCertificate> findAllWithSortingFiltering(SortParamsContext sortParamsContext,
                                                       List<Long> ids, String partInfo);
+
+    /**
+     * Updates Certificate by id by map of update info
+     *
+     * @param id                 Certificate id to search
+     * @param giftCertificateInfo Update information with Certificate fields and values
+     */
     void updateById(long id, Map<String, Object> giftCertificateInfo);
+
+    /**
+     * Finds Certificate by id.
+     *
+     * @param id Certificate id to search
+     * @return Optional Certificate - Certificate if founded or Empty if not
+     */
     Optional<GiftCertificate> findById(long id);
+
+    /**
+     * Finds Certificate by name.
+     *
+     * @param name Certificate name to search
+     * @return Optional Certificate - Certificate if founded or Empty if not
+     */
     Optional<GiftCertificate> findByName(String name);
+
+    /**
+     * Deletes Certificate by id.
+     *
+     * @param id Certificate id to search
+     */
     void deleteById(long id);
 }
--- src/main/java/com/epam/esm/service/GiftCertificateService.java
+++ src/main/java/com/epam/esm/service/GiftCertificateService.java
@@ -1,15 +1,71 @@
 package com.epam.esm.service;
 
+import com.epam.esm.exception.InvalidEntityException;
+import com.epam.esm.exception.NotFoundEntityException;
 import com.epam.esm.model.dto.GiftCertificateDto;
 import com.epam.esm.model.entity.GiftCertificate;
 
 import java.util.List;
 
+/**
+ * Business logic interface for Certificates.
+ */
 public interface GiftCertificateService {
+    /**
+     * Creates new Certificate with optional tags.
+     *
+     * @param giftCertificateDto CertificateDto to create Certificate/Tags
+     * @return id location of new Certificate
+     * @throws InvalidEntityException when Certificate or Tag data is invalid
+     */
     long create(GiftCertificateDto giftCertificateDto);
+
+    /**
+     * Find all Certificates.
+     *
+     * @return List of all Certificates
+     */
     List<GiftCertificate> findAll();
+
+    /**
+     * Find Certificate by id.
+     *
+     * @param id Certificate id to search
+     * @return founded Certificate
+     * @throws NotFoundEntityException when Certificate is not found
+     */
     GiftCertificate findById(long id);
+
+    /**
+     * Updates Certificate by id
+     * with updating only fields that are passed
+     *
+     * @param id              Certificate id to search
+     * @param giftCertificateDto update information with Certificate fields or Tags
+     * @return updated Certificate with Tags
+     * @throws NotFoundEntityException when Certificate is not found
+     * @throws InvalidEntityException when update info of Certificate fields is invalid
+     */
     GiftCertificateDto updateById(long id, GiftCertificateDto giftCertificateDto);
+
+    /**
+     * Deletes Certificate by id.
+     *
+     * @param id Certificate id to search
+     * @throws NotFoundEntityException when Certificate is not found
+     */
     void deleteById(long id);
+
+    /**
+     * Find all Certificates with Tags and optional sorting/filtering
+     *
+     * @param tagName     Tag name to filter Certificates
+     * @param partInfo    part info of name/desc to filter Certificates
+     * @param sortColumns columns to sort of Certificates
+     * @param orderTypes  sort order types
+     * @return List of sorted/filtered Certificates with Tags
+     * @throws NotFoundEntityException when Tag is not found
+     * @throws InvalidEntityException when sort parameters are invalid
+     */
     List<GiftCertificateDto> findAllWithTags(String tagName, String partInfo, List<String> sortColumns, List<String> orderTypes);
 }
