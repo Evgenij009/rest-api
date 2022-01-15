d06e93a532516e137c249601836970155f1dcfcb
Evgenij009 <evgeshashadura@gmail.com>
2022-01-15 20:40:44 +0300

    some fixed

--- src/main/java/com/epam/esm/util/security/jwt/JwtUser.java
+++ src/main/java/com/epam/esm/util/security/jwt/JwtUser.java
@@ -31,10 +31,6 @@ public class JwtUser implements UserDetails {
         return id;
     }
 
-    public String getLogin() {
-        return login;
-    }
-
     public String getName() {
         return name;
     }
